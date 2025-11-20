b84094dd670799f78a56bf373c4aad298b08ea6e
rohit <gutrrohit@gmail.com>
2025-11-21 00:28:35 +0530

    Asynchronous running the webscrapper

--- modal/webScrapper.py
+++ modal/webScrapper.py
@@ -2,15 +2,39 @@ import re
 import sys
 import urllib.request
 
-def get_links(url):
-    response = urllib.request.urlopen(url)
-    html = response.read().decode("utf8")
-    links = []
-    for match in re.finditer('href="(.*?)"', html):
-        links.append(match.group(1))
-    
-    return links
+import modal
 
-if __name__ == "__main__":
-    links = get_links(sys.argv[1])
-    print(links)
+app = modal.App(name="webscrapper")
+
+playwright_image = modal.Image.debian_slim(python_version="3.10").run_commands(
+    "apt-get update",
+    "apt-get install -y software-properties-common",
+    "apt-add-repository non-free",
+    "apt-add-repository contrib",
+    "pip install playwright==1.42.0",
+    "playwright install-deps chromium",
+    "playwright install chromium",
+)
+
+@app.function(image=playwright_image)
+async def get_links(cur_url: str) -> list[str]:
+    from playwright.async_api import async_playwright
+
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+        page = await browser.new_page()
+        await page.goto(cur_url)
+        links = await page.eval_on_selector_all(
+            "a[href]", "elements => elements.map(element => element.href)"
+        )
+        await browser.close()
+    
+    print("Links", links)
+    return list(set(links))
+
+@app.local_entrypoint()
+def main():
+    urls = ["http://modal.com", "http://github.com"]
+    for links in get_links.map(urls):
+        for link in links:
+            print(link)
